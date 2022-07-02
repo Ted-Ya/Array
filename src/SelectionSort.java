c4edeb85d4aa0188e1eb6ecadce801dc0afeaa46
Ted-Ya <Ted_ya@163.com>
2022-07-02 16:51:50 +0800

    first commit. Add Selection Sort

---
+++ src/SelectionSort.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+/**
+ * Sort an array ,lets elements of this array in ascending
+ */
+public class SelectionSort {
+
+    public static void main(String[] args) {
+       int[] arr ={5,4,3,2,1};
+       selectionSort(arr);
+       System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * every traversal of this arr ,we will find the minor number,and put it in the right position
+     * @param arr need to be sorted
+     */
+    private static void selectionSort(int[] arr){
+      if(arr == null || arr.length <2){
+          return;
+      }
+
+      //first time arrange : 0~n-1
+      //sec time arrange : 1~ n-2
+      for(int i=0;i< arr.length;i++){
+          for(int j =i;j-1>=0&&j<arr.length;j--){
+              if(arr[j-1]>arr[j]){
+                  swap(arr,j-1,j);
+              }
+          }
+      }
+    }
+
+    private static void swap(int[] arr,int indexX, int indexY){
+        int temp = arr[indexX];
+        arr[indexX] = arr[indexY];
+        arr[indexY] = temp;
+    }
+}
