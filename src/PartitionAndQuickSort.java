8b430d0ce7478e6736d39936c72256cc6aea79a9
Ted-Ya <Ted_ya@163.com>
2022-07-18 23:21:38 +0800

    1.modify patition2 mothod 2.add quick sort

--- src/PartitionAndQuickSort.java
+++ src/PartitionAndQuickSort.java
@@ -4,21 +4,31 @@ public class PartitionAndQuickSort {
 //        int[] arr = {5,3,7,2,3,4,1};
 //        partition(arr,3);
         int[] arr = {3,5,4,0,4,5,6,8,7,5,9,4,2,1,2,3,5,9,4,5,8,5};
-        partition2(arr,5);
+//        partition2(arr,5);
+        quickSort(arr,0, arr.length-1);
         for(int a:arr){
             System.out.print(a + " ");
         }
     }
 
+    private static void quickSort(int[] arr,int L ,int R){
+        if(L >=R){
+            return;
+        }
+        swap(arr,L+(int)(Math.random()*(R-L+1)),R);
+        int[] equalArea =partition2(arr,arr[R]);
+        quickSort(arr,L,equalArea[0]-1);
+        quickSort(arr,equalArea[1]+1,R);
+    }
     /**
      * To divide this arr into three parties ,left part is min than num the middle part is equal the num,
      * and right part is more than num
      * @param arr numbers arr
      * @param num specific num
      */
-    private static void partition2(int[] arr,int num){
+    private static int[] partition2(int[] arr,int num){
         if(arr == null || arr.length <2){
-            return;
+            return new int[]{-1,-1};
         }
 
         int leftIndex = -1;
@@ -28,14 +38,14 @@ public class PartitionAndQuickSort {
             if(arr[i] == num){
                 i++;
             } else if(arr[i] < num){
-                leftIndex ++;
+                ++leftIndex;
                 swap(arr,leftIndex,i);
                 i++;
             }else if(arr[i] > num){
-                rightIndex --;
-                swap(arr,i,rightIndex);
+                swap(arr,i,--rightIndex);
             }
         }
+        return new int[]{leftIndex+1,rightIndex-1};
     }
 
     /**
